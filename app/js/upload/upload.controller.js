dfff5d48aa1ef05b4c7b742c88fd3edceb30d221
Josh Herder <jdherder@gmail.com>
2015-11-27 16:10:27 -0600

    Begin laying angular framework for first feature.

---
+++ app/js/upload/upload.controller.js
@@ -0,0 +1,12 @@
+(function() {
+    angular
+        .module('app.upload')
+        .controller('UploadController', UploadController);
+
+    //UploadController.$inject = [];
+
+    function UploadController() {
+
+    }
+
+})();
